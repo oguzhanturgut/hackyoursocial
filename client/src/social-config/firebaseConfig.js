f8a5c16070e3509c774ed4fc270c623de5330784
Gul <gulpembeyener@gmail.com>
2019-11-26 22:22:39 +0100

    Remove config

--- client/src/social-config/firebaseConfig.js
+++ client/src/social-config/firebaseConfig.js
@@ -3,14 +3,14 @@ import * as firebase from 'firebase';
 // const settings = { timestampsInSnapshots: true };
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDE8pABtUCH2kzGsN-htLQY8yDednivRiQ',
-  authDomain: 'ocean-d27d7.firebaseapp.com',
-  databaseURL: 'https://ocean-d27d7.firebaseio.com',
-  projectId: 'ocean-d27d7',
-  storageBucket: 'ocean-d27d7.appspot.com',
-  messagingSenderId: '732932278090',
-  appId: '1:732932278090:web:2f6389b5472052698663b3',
-  measurementId: 'G-3X88ZLXK9L',
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
 };
 
 firebase.initializeApp(firebaseConfig);
