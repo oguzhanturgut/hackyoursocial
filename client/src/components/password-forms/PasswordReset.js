9e355c64d9a667c30cc625c373623397d4d8846b
musafir928 <uganoghli@gmail.com>
2019-11-28 20:55:14 +0100

    password reset issue (with invalid link) solved

--- client/src/components/password-forms/PasswordReset.js
+++ client/src/components/password-forms/PasswordReset.js
@@ -1,53 +1,75 @@
-import React, { Fragment, useState } from 'react';
-// import { Redirect } from "react-router-dom";
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import axios from 'axios';
-import { setAlert } from '../../actions/alert';
-import PropTypes from 'prop-types';
+import React, { Fragment, useState } from "react";
+import { Redirect } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import axios from "axios";
+import { setAlert } from "../../actions/alert";
+import PropTypes from "prop-types";
 
 const PasswordReset = ({ match, setAlert }) => {
   const [formData, setFormData] = useState({
-    password: '',
-    password2: ''
+    password: "",
+    password2: ""
   });
 
   const { password, password2 } = formData;
   const [redirect, setRedirect] = useState(false);
+  const [resetError, setResetError] = useState(false);
 
   const onChange = e =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
   const resetPasswordLink = match.params.token;
-  console.log(resetPasswordLink);
   const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
-      setAlert('Passwords do not match', 'danger');
-      e.target.password.value = '';
-      e.target.password2.value = '';
+      setAlert("Passwords do not match", "danger");
+      e.target.password.value = "";
+      e.target.password2.value = "";
     } else {
-      setRedirect(true);
-      const reqConfig = {
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      };
+      try {
+        const reqConfig = {
+          headers: {
+            "Content-Type": "application/json"
+          }
+        };
 
-      const reqBody = JSON.stringify({
-        resetPasswordLink,
-        newPassword: formData.password
-      });
+        const reqBody = JSON.stringify({
+          resetPasswordLink,
+          newPassword: formData.password
+        });
 
-      await axios.put(`/api/auth/reset-password`, reqBody, reqConfig);
+        await axios.put(`/api/auth/reset-password`, reqBody, reqConfig);
+        setRedirect(true);
+      } catch (err) {
+        console.error(err.message);
+        setAlert("Password Reset Link Invalid", "danger");
+        setResetError(true);
+      }
     }
   };
+
+  if (resetError) {
+    return (
+      <>
+        {" "}
+        <p className='my-1'>
+          Resend Recovery Email?{" "}
+          <Link to='/email-password'>Resend password recovery email</Link>
+        </p>
+        <p className='my-1'>
+          Login? <Link to='/login'>Login</Link>
+        </p>
+      </>
+    );
+  }
+
   if (redirect) {
     // return <Redirect to='/login' />;
-    setAlert(`Password Reset success`, 'success');
+    setAlert(`Password Reset success`, "success");
     return (
-      <p className="my-1">
-        Password Reset Success{' '}
-        <Link to="/login" className="btn btn-primary">
+      <p className='my-1'>
+        Password Reset Success{" "}
+        <Link to='/login' className='btn btn-primary'>
           Login
         </Link>
       </p>
@@ -56,31 +78,31 @@ const PasswordReset = ({ match, setAlert }) => {
 
   return (
     <Fragment>
-      <h1 className="large text-primary">Reset Password</h1>
-      <p className="lead">
-        <i className="fas fa-user" /> Input your new Password
+      <h1 className='large text-primary'>Reset Password</h1>
+      <p className='lead'>
+        <i className='fas fa-user' /> Input your new Password
       </p>
-      <form className="form" onSubmit={e => onSubmit(e)}>
-        <div className="form-group">
+      <form className='form' onSubmit={e => onSubmit(e)}>
+        <div className='form-group'>
           <input
-            type="password"
-            placeholder="Password"
-            name="password"
+            type='password'
+            placeholder='Password'
+            name='password'
             onChange={e => onChange(e)}
             required
           />
         </div>
-        <div className="form-group">
+        <div className='form-group'>
           <input
-            type="password"
-            placeholder="Repeat your password"
-            name="password2"
+            type='password'
+            placeholder='Repeat your password'
+            name='password2'
             onChange={e => onChange(e)}
             required
           />
         </div>
 
-        <input type="submit" className="btn btn-primary" value="Submit" />
+        <input type='submit' className='btn btn-primary' value='Submit' />
       </form>
     </Fragment>
   );
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,26 +1,26 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const auth = require('../../middleware/auth');
-const jwt = require('jsonwebtoken');
-const config = require('config');
-const { sendEmail } = require('../../send_mail');
-const { check, validationResult } = require('express-validator/check');
+const bcrypt = require("bcryptjs");
+const auth = require("../../middleware/auth");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const { sendEmail } = require("../../send_mail");
+const { check, validationResult } = require("express-validator/check");
 
-const HOST_ADDR = process.env.HOST_ADDR || config.get('local_url');
+const HOST_ADDR = process.env.HOST_ADDR || config.get("local_url");
 
-const User = require('../../models/User');
+const User = require("../../models/User");
 
 // @route    GET api/auth
 // @desc     Test route
 // @access   Public
-router.get('/', auth, async (req, res) => {
+router.get("/", auth, async (req, res) => {
   try {
-    const user = await User.findById(req.user.id).select('-password');
+    const user = await User.findById(req.user.id).select("-password");
     res.json(user);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Server Error');
+    res.status(500).send("Server Error");
   }
 });
 
@@ -28,10 +28,10 @@ router.get('/', auth, async (req, res) => {
 // @desc     Authenticate user & get token
 // @access   Public
 router.post(
-  '/',
+  "/",
   [
-    check('email', 'Please include a valid email').isEmail(),
-    check('password', 'Password is required').exists()
+    check("email", "Please include a valid email").isEmail(),
+    check("password", "Password is required").exists()
   ],
   async (req, res) => {
     const errors = validationResult(req);
@@ -47,7 +47,7 @@ router.post(
       if (!user) {
         return res
           .status(400)
-          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
       }
 
       const isMatch = await bcrypt.compare(password, user.password);
@@ -55,13 +55,13 @@ router.post(
       if (!isMatch) {
         return res
           .status(400)
-          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+          .json({ errors: [{ msg: "Invalid Credentials" }] });
       }
 
       if (!user.confirmed)
         return res
           .status(400)
-          .json({ errors: [{ msg: 'Please confirm your email to login' }] });
+          .json({ errors: [{ msg: "Please confirm your email to login" }] });
 
       const payload = {
         user: {
@@ -71,7 +71,7 @@ router.post(
 
       jwt.sign(
         payload,
-        config.get('jwtSecret'),
+        config.get("jwtSecret"),
         { expiresIn: 360000 },
         (err, token) => {
           if (err) throw err;
@@ -80,7 +80,7 @@ router.post(
       );
     } catch (err) {
       console.error(err.message);
-      res.status(500).send('Server error');
+      res.status(500).send("Server error");
     }
   }
 );
@@ -88,41 +88,37 @@ router.post(
 // password forgot and reset routes**************
 // password forgot and reset routes*************
 router.put(
-  '/forgot-password',
-  [check('email', 'Please include a valid email').isEmail()],
+  "/forgot-password",
+  [check("email", "Please include a valid email").isEmail()],
   (req, res) => {
-    console.log('forget password');
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    console.log('forgot password finding user with that email');
     const { email } = req.body;
-    console.log('signin req.body', email);
 
     // find the user in our database based on email
     User.findOne({ email }, (err, user) => {
       // if err or no user
       if (err || !user)
-        return res.status('401').json({
-          error: 'User with that email does not exist!'
+        return res.status("401").json({
+          error: "User with that email does not exist!"
         });
 
       // generate a token with user id and secret
-      const token = jwt.sign({ _id: user._id }, config.get('jwtSecret'));
+      const token = jwt.sign({ _id: user._id }, config.get("jwtSecret"));
       // res.json({ user, token });
 
       // email data
       const resetPwUrl = `${HOST_ADDR}/reset-password/${token}`;
       const emailData = {
-        from: process.env.MAIL_USER || config.get('email'),
+        from: process.env.MAIL_USER || config.get("email"),
         to: email,
-        subject: 'Password Reset Instructions',
+        subject: "Password Reset Instructions",
         text: `Please use the following link to reset your password: ${resetPwUrl}`,
         html: `<p>Please use the following link to reset your password:</p> <p>${resetPwUrl}</p>`
       };
-      console.log(emailData.text);
 
       return user.updateOne({ resetPasswordLink: token }, (err, success) => {
         if (err) {
@@ -140,11 +136,11 @@ router.put(
 
 // ***
 router.put(
-  '/reset-password',
+  "/reset-password",
   [
     check(
-      'newPassword',
-      'Please enter a password with 6 or more characters'
+      "newPassword",
+      "Please enter a password with 6 or more characters"
     ).isLength({ min: 6 })
   ],
   async (req, res) => {
@@ -156,19 +152,19 @@ router.put(
 
     try {
       const { resetPasswordLink, newPassword } = req.body;
-      console.log(resetPasswordLink, newPassword);
+
       let user = await User.findOne({ resetPasswordLink });
       // if  Not the user
       if (!user)
-        return res.status('401').json({
-          error: 'Invalid Link!'
+        return res.status("401").json({
+          error: "Invalid Link!"
         });
       // there is a user
       const salt = await bcrypt.genSalt(10);
       const newPasswordCrypt = await bcrypt.hash(newPassword, salt);
       user = await User.findOneAndUpdate(
         { resetPasswordLink },
-        { $set: { resetPasswordLink: '', password: newPasswordCrypt } }
+        { $set: { resetPasswordLink: "", password: newPasswordCrypt } }
       );
 
       res.json({
