230bebf152b9abb19c402381161e01ceb844702e
20twa07 <48910851+20twa07@users.noreply.github.com>
2019-11-29 17:02:52 +0100

    add social user model

--- models/User.js
+++ models/User.js
@@ -1,26 +1,38 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
+  user: {
+    name: {
+      type: String,
+    },
+    email: {
+      type: String,
+      unique: true,
+    },
+    avatar: {
+      type: String,
+    },
+  },
   name: {
     type: String,
-    required: true
+    required: true,
   },
   email: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
   password: {
     type: String,
-    required: true
+    required: true,
   },
   avatar: {
-    type: String
+    type: String,
   },
   date: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
 });
 
 module.exports = User = mongoose.model('user', UserSchema);
