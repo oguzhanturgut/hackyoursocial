1316db1b51dc7eea033ffa829fd6d15a1bcbb393
20twa07 <48910851+20twa07@users.noreply.github.com>
2019-11-30 10:49:29 +0100

    add jwt

--- routes/api/users.js
+++ routes/api/users.js
@@ -93,8 +93,22 @@ router.post('/facebook', async (req, res) => {
       avatar,
       password,
     });
+    const salt = await bcrypt.genSalt(10);
+
+    user.password = await bcrypt.hash(password, salt);
+
     await user.save();
-    res.json({ password });
+
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 360000 }, (err, token) => {
+      if (err) throw err;
+      res.json({ token });
+    });
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server error');
