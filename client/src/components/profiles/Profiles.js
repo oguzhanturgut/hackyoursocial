e802ca4b5c2d4ce3643a3944822e946cc380abd7
20twa07 <48910851+20twa07@users.noreply.github.com>
2019-12-04 11:08:54 +0100

    refactor for pagination

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import ProfileItem from './ProfileItem';
 import { getProfiles } from '../../actions/profile';
+import Pagination from './Pagination';
 
 const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
   useEffect(() => {
@@ -13,9 +14,21 @@ const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
   const [searchField, setSearchField] = useState('');
 
   const filteredProfiles = profiles.filter(profile =>
-    profile.user.name.toLowerCase().includes(searchField.toLowerCase())
+    profile.user.name.toLowerCase().includes(searchField.toLowerCase()),
   );
   console.log(filteredProfiles);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [profilesPerPage] = useState(5);
+
+  // Get current posts
+  const indexOfLastProfile = currentPage * profilesPerPage;
+  const indexOfFirstProfile = indexOfLastProfile - profilesPerPage;
+  const currentProfiles = filteredProfiles.slice(indexOfFirstProfile, indexOfLastProfile);
+  console.log(currentProfiles);
+
+  // Change page
+  const paginate = pageNumber => setCurrentPage(pageNumber);
 
   return (
     <Fragment>
@@ -23,21 +36,23 @@ const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
         <Spinner />
       ) : (
         <Fragment>
-          <h1 className="large text-primary">Developers</h1>
-          <p className="lead">
-            <i className="fab fa-connectdevelop" /> Browse and connect with
-            developers
+          <h1 className='large text-primary'>Developers</h1>
+          <p className='lead'>
+            <i className='fab fa-connectdevelop' /> Browse and connect with developers
           </p>
           <input
-            type="search"
-            placeholder="seach for developers"
+            type='search'
+            placeholder='search for developers'
             onChange={e => setSearchField(e.target.value)}
           />
-          <div className="profiles">
+          <Pagination
+            profilesPerPage={profilesPerPage}
+            totalProfiles={filteredProfiles.length}
+            paginate={paginate}
+          />
+          <div className='profiles'>
             {filteredProfiles.length > 0 ? (
-              filteredProfiles.map(profile => (
-                <ProfileItem key={profile._id} profile={profile} />
-              ))
+              currentProfiles.map(profile => <ProfileItem key={profile._id} profile={profile} />)
             ) : (
               <h4>No profiles found...</h4>
             )}
@@ -50,11 +65,11 @@ const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
 
 Profiles.propTypes = {
   getProfiles: PropTypes.func.isRequired,
-  profile: PropTypes.object.isRequired
+  profile: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = state => ({
-  profile: state.profile
+  profile: state.profile,
 });
 
 export default connect(mapStateToProps, { getProfiles })(Profiles);
