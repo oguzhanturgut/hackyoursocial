96ec0b0bec9045e05b3fb62a40873656cdd99dc3
musafir928 <uganoghli@gmail.com>
2019-12-11 17:28:18 +0100

    change config

--- client/src/social-config/firebaseConfig.js
+++ client/src/social-config/firebaseConfig.js
@@ -1,17 +1,15 @@
-import * as firebase from 'firebase';
+import * as firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: '',
-  authDomain: '',
-  databaseURL: '',
-  projectId: '',
-  storageBucket: '',
-  messagingSenderId: '',
-  appId: '',
-  measurementId: '',
+  apiKey: "AIzaSyAIOjdj3MF7tlqCpU435u3ChU7E9t7FpoM",
+  authDomain: "hyf-project-48ef4.firebaseapp.com",
+  databaseURL: "https://hyf-project-48ef4.firebaseio.com",
+  projectId: "hyf-project-48ef4",
+  storageBucket: "hyf-project-48ef4.appspot.com",
+  messagingSenderId: "111507724756",
+  appId: "1:111507724756:web:a6d88666bc9fad05239e41",
+  measurementId: "G-ZN6XZ9FDZE"
 };
-
 // Could not remove this into another file it breaks the code
 const firebaseApp = firebase.initializeApp(firebaseConfig);
-
 export default { firebaseApp, firebaseConfig };
